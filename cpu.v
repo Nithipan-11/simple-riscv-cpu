23186ad2c2e7fd55e00c3bbe2531d5187d64a93c
Nithipan Sivakanthan <nithipan123@gmail.com>
2026-08-19 11:05:41 -0400

    Fix B-type branch immediate bug and corrupted lw/beq encoding in program.hex

--- cpu.v
+++ cpu.v
@@ -68,7 +68,7 @@ module cpu (
     // Sign-extended immediate values for I-type and S-type instructions.
     wire [31:0] imm_i = {{20{instr[31]}}, instr[31:20]};
     wire [31:0] imm_s = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-    wire [31:0] imm_b = {{19{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+    wire [31:0] imm_b = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
 
     // ==========================
     // Register file
